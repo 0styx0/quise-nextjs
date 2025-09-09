926f23df9d316f8b6f809d137b0ccdc2ad27a604
Styx Meiseles <git@0styx0.xyz>
2025-09-09 19:04:17 -0400

    feat(api): use apollo nextjs integration
    https://github.com/apollographql/apollo-client-integrations/tree/main/packages/nextjs

--- src/components/app/ClientProviders.tsx
+++ src/components/app/ClientProviders.tsx
@@ -2,16 +2,16 @@
 "use client";
 
 import { CartProvider } from "@/context/cartContext";
-import { graphqlClient } from "@/lib/apolloClient";
-import { ApolloProvider } from "@apollo/client/react";
+import { makeClient } from "@/lib/apollo/apolloClient";
+import { ApolloNextAppProvider } from "@apollo/client-integration-nextjs";
 import { ReactNode } from "react";
 
 export const ClientProviders = ({ children }: { children: ReactNode }) => {
     return (
-        <ApolloProvider client={graphqlClient}>
+        <ApolloNextAppProvider makeClient={makeClient}>
           <CartProvider>
           {children}
           </CartProvider>
-        </ApolloProvider>
+        </ApolloNextAppProvider>
     )
 };
--- src/lib/helpers/getProducts.ts
+++ src/lib/helpers/getProducts.ts
@@ -1,5 +1,4 @@
 import { GetProductsQuery } from "@/generated/graphql/graphql"
-import { graphqlClient } from "@/lib/apolloClient"
 import { GET_PRODUCTS } from "@/lib/queries/getProducts"
 
 export async function getProducts() {
